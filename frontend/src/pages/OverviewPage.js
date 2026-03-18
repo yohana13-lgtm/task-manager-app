08f82211c9d165f73afe69c63d9ed1571b292518
yohana13-lgtm <145039827+yohana13-lgtm@users.noreply.github.com>
2026-03-18 14:48:00 +0700

    fix: disable eslint warning for vercel build

--- frontend/src/pages/OverviewPage.js
+++ frontend/src/pages/OverviewPage.js
@@ -18,6 +18,7 @@ function OverviewPage({ user }) {
 
   useEffect(() => {
     loadTasks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const loadTasks = async () => {
